972e053c862e3870114618818b758b69cdde8635
Suroor Hussain <suroorhussain@hamon.in>
2024-09-01 13:29:03 +0530

    Allow user to login using their username

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from .views import SignupAPIView
 
 urlpatterns = [
     path('signup/', SignupAPIView.as_view(), name='account_signup'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
