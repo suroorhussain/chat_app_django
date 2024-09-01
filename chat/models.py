a92afa0ff959f07629d995812cfe49f794d89435
Suroor Hussain <surusuroor@gmail.com>
2024-09-01 19:38:43 +0530

    Create models to store chats

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,24 @@
+from uuid import uuid4
+
 from django.db import models
 
-# Create your models here.
+class Conversations(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    members = models.ManyToManyField('account.User', related_name='conversations')
+
+    class Meta:
+        ordering = ['-updated_at']
+
+class Messages(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    conversation = models.ForeignKey(Conversations, on_delete=models.CASCADE, related_name='messages')
+    sender = models.ForeignKey('account.User', on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_at']
