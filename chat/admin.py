8e01364a4a9a493be163aa18163427b8e8feba59
Suroor Hussain <suroorhussain@hamon.in>
2024-09-01 21:03:24 +0530

    Allow users to chat when connected to a group

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.site.register(models.Messages)
+admin.site.register(models.Conversations)
--- chat/consumers.py
+++ chat/consumers.py
@@ -4,26 +4,31 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 from asgiref.sync import async_to_sync
 
+from . import utils
+
+from pprint import pprint
+
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        # self.room_name = self.scope['url_route']['kwargs']['room_name']
+        pprint(self.scope['url_route']['kwargs'])
         user = self.scope["user"]
         if user.is_anonymous:
             await self.close()
-        self.room_group_name = f'chat_{self.room_name}'
+        self.conversation_id = str(await utils.get_conversation_id(user, **self.scope['url_route']['kwargs']))
 
-        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        await self.channel_layer.group_add(self.conversation_id, self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        await self.channel_layer.group_discard(self.conversation_id, self.channel_name)
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat.message", "message": message}
+            self.conversation_id, {"type": "chat.message", "message": message}
         )
 
     async def chat_message(self, event):
