88681c0885a4b8d83aa71097d8771189f1a1578c
Suroor Hussain <surusuroor@gmail.com>
2024-09-01 21:03:24 +0530

    Allow users to chat when connected to a group

--- chat/ws_urls.py
+++ chat/ws_urls.py
@@ -4,4 +4,6 @@ from . import consumers
 
 urlpatterns = [
     re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/chat/dm/(?P<username>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/chat/group/(?P<groupid>[0-9a-f-]+)/$', consumers.ChatConsumer.as_asgi()),
 ]
