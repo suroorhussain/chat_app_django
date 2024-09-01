f63453fe6a5c660f68937fb0560269f66d9b8974
Suroor Hussain <surusuroor@gmail.com>
2024-09-02 00:33:56 +0530

    Allow retrieval of messages in a conversation

--- chat/serializers.py
+++ chat/serializers.py
@@ -5,7 +5,27 @@ from .models import (
     Messages
 )
 
+from account.models import User
+
+class MemberSerisalizer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'avatar']
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender = MemberSerisalizer()
+    class Meta:
+        model = Messages
+        fields = ['sender', 'message', 'id', 'created_at']
+
 class ConversationSerializer(serializers.ModelSerializer):
+    members = MemberSerisalizer(many=True)
     class Meta:
         model = Conversations
-        fields = ['name', 'type', 'members', 'id']
+        fields = [
+            'name',
+            'type',
+            'members',
+            'id',
+            'unread_messages',
+        ]
--- chat/views.py
+++ chat/views.py
@@ -1,27 +1,38 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import ListAPIView
 from rest_framework.permissions import (
     BasePermission,
     IsAuthenticated
 )
 
-from .serializers import ConversationSerializer
+from . import serializers
+from . import models
 
 class IsMember(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return request.user in obj.members.all()
+    def has_object_permission(self, request, view):
+        print("in persmission class")
+        try:
+            conversation = models.Conversations.objects.get(id=view.kwargs['pk'])
+        except models.Conversations.DoesNotExist:
+            return False
+        return request.user in conversation.members.all()
 
 class ConversationViewSet(ModelViewSet):
 
+    serializer_class = serializers.ConversationSerializer
     def get_queryset(self):
         return self.request.user.conversations.all()
-    
-    def get_serializer_class(self):
-        if self.action == 'create':
-            return ConversationSerializer
-        return ConversationSerializer
     
     def get_permissions(self):
         if self.action in ['list', 'create']:
             return [IsAuthenticated()]
         else:
             return [IsAuthenticated(), IsMember()]
+        
+class ListMessageView(ListAPIView):
+    serializer_class = serializers.MessageSerializer
+    permission_classes = [IsAuthenticated & IsMember]
+    
+    def get_queryset(self):
+        conversation_id = self.kwargs['pk']
+        return self.request.user.conversations.get(id=conversation_id).messages.all()
