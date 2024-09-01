01d340c084245efa3b00926e4d89fcf2a7a4a01d
Suroor Hussain <surusuroor@gmail.com>
2024-09-01 15:21:57 +0530

    Add channels and daphne for chat development

--- chat_app/asgi.py
+++ chat_app/asgi.py
@@ -11,6 +11,12 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+from channels.routing import ProtocolTypeRouter
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app.settings')
 
-application = get_asgi_application()
+django_application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": django_application,
+})
