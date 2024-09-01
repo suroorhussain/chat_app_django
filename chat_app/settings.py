2f880e9b65d7b8cf9ecdb9a2a4b95a8d41fed580
Suroor Hussain <surusuroor@gmail.com>
2024-09-01 13:29:03 +0530

    Allow user to login using their username

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -125,3 +125,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'account.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    )
+}
